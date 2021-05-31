53e0e7e1e2537a3a04ee164ba3bf920c0215c4fc
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2021-06-01 00:11:13 +0530

    Add projects for Chapter 6

---
+++ 06-room-architecture/projects/final/app/src/androidTest/java/com/raywenderlich/droidquiz/ExampleInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.raywenderlich.droidquiz
+
+import androidx.test.runner.AndroidJUnit4
+import com.raywenderlich.droidquiz.data.db.QuestionDao
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+  /*@Test
+  fun useAppContext() {
+      // Context of the app under test.
+      val appContext = InstrumentationRegistry.getTargetContext()
+      assertEquals("com.raywenderlich.droidquiz", appContext.packageName)
+  }*/
+}
