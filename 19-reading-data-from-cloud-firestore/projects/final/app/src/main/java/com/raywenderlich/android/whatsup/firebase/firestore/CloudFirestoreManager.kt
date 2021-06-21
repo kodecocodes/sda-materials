af20d8fbc6e90567e2d294c8ce80960c270ef9c4
Harun Wangereka <harunwangereka@gmail.com>
2021-06-21 12:21:32 +0300

    Package Name change

---
+++ 19-reading-data-from-cloud-firestore/projects/final/app/src/main/java/com/raywenderlich/android/whatsup/firebase/firestore/CloudFirestoreManager.kt
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.whatsup.firebase.firestore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.raywenderlich.android.whatsup.firebase.authentication.AuthenticationManager
+import com.raywenderlich.android.whatsup.model.Comment
+import com.raywenderlich.android.whatsup.model.Post
+
+private const val POSTS_COLLECTION = "posts"
+private const val COMMENTS_COLLECTION = "comments"
+
+private const val AUTHOR_KEY = "author"
+private const val CONTENT_KEY = "content"
+private const val ID_KEY = "id"
+private const val TIMESTAMP_KEY = "timestamp"
+private const val POST_ID = "post_id"
+
+class CloudFirestoreManager {
+
+  private val authenticationManager = AuthenticationManager()
+  private val database = FirebaseFirestore.getInstance()
+
+  private val postsValues = MutableLiveData<List<Post>>()
+  private val commentsValues = MutableLiveData<List<Comment>>()
+
+  private lateinit var postsRegistration: ListenerRegistration
+  private lateinit var commentsRegistration: ListenerRegistration
+
+  fun addPost(content: String, onSuccessAction: () -> Unit, onFailureAction: () -> Unit) {
+    //1
+    val documentReference = database.collection(POSTS_COLLECTION).document()
+
+    //2
+    val post = HashMap<String, Any>()
+
+    //3
+    post[AUTHOR_KEY] = authenticationManager.getCurrentUser()
+    post[CONTENT_KEY] = content
+    post[TIMESTAMP_KEY] = getCurrentTime()
+    post[ID_KEY] = documentReference.id
+
+    //4
+    documentReference
+        .set(post)
+        .addOnSuccessListener { onSuccessAction() }
+        .addOnFailureListener { onFailureAction() }
+  }
+
+  fun onPostsValuesChange(): LiveData<List<Post>> {
+    listenForPostsValueChanges()
+    return postsValues
+  }
+
+  fun stopListeningForPostChanges() = postsRegistration.remove()
+
+  fun updatePostContent(key: String, content: String, onSuccessAction: () -> Unit, onFailureAction: () -> Unit) {
+    //1
+    val updatedPost = HashMap<String, Any>()
+
+    //2
+    updatedPost[CONTENT_KEY] = content
+
+    //3
+    database.collection(POSTS_COLLECTION)
+        .document(key)
+        .update(updatedPost)
+        .addOnSuccessListener { onSuccessAction() }
+        .addOnFailureListener { onFailureAction() }
+  }
+
+  fun deletePost(key: String, onSuccessAction: () -> Unit, onFailureAction: () -> Unit) {
+    database.collection(POSTS_COLLECTION)
+        .document(key)
+        .delete()
+        .addOnSuccessListener { onSuccessAction() }
+        .addOnFailureListener { onFailureAction() }
+
+    deletePostComments(key)
+  }
+
+  fun addComment(postId: String, content: String, onSuccessAction: () -> Unit, onFailureAction: () -> Unit) {
+    // 1
+    val commentReference = database.collection(COMMENTS_COLLECTION).document()
+
+    // 2
+    val comment = HashMap<String, Any>()
+
+    // 3
+    comment[AUTHOR_KEY] = authenticationManager.getCurrentUser()
+    comment[CONTENT_KEY] = content
+    comment[POST_ID] = postId
+    comment[TIMESTAMP_KEY] = getCurrentTime()
+
+    // 4
+    commentReference
+        .set(comment) // 5
+        .addOnSuccessListener { onSuccessAction() } // 6
+        .addOnFailureListener { onFailureAction() } // 7
+  }
+
+  fun onCommentsValuesChange(postId: String): LiveData<List<Comment>> {
+    listenForPostCommentsValueChanges(postId)
+    return commentsValues
+  }
+
+  fun stopListeningForCommentsChanges() = commentsRegistration.remove()
+
+  private fun deletePostComments(postId: String) {
+    // 1
+    database.collection(COMMENTS_COLLECTION)
+        .whereEqualTo(POST_ID, postId)
+        //2
+        .get()
+        //3
+        .continueWith { task -> task.result?.documents?.forEach { it.reference.delete() } }
+  }
+
+  private fun listenForPostCommentsValueChanges(postId: String) {
+    // 1
+    commentsRegistration = database.collection(COMMENTS_COLLECTION)
+        // 2
+        .whereEqualTo(POST_ID, postId) // 3
+        // 4
+        .addSnapshotListener(EventListener { value, error ->
+          if (error != null || value == null) {
+            return@EventListener
+          }
+
+          if (value.isEmpty) {
+            postsValues.postValue(emptyList())
+          } else {
+            val comments = ArrayList<Comment>()
+            for (doc in value) {
+              val comment = doc.toObject(Comment::class.java)
+              comments.add(comment)
+            }
+            commentsValues.postValue(comments)
+          }
+        })
+  }
+
+  private fun listenForPostsValueChanges() {
+    // 1
+    postsRegistration = database.collection(POSTS_COLLECTION) // 2
+        // 3
+        .addSnapshotListener(EventListener { value, error ->
+          // 4
+          if (error != null || value == null) {
+            return@EventListener
+          }
+
+          // 5
+          if (value.isEmpty) {
+            // 6
+            postsValues.postValue(emptyList())
+          } else {
+            // 7
+            val posts = ArrayList<Post>()
+            // 8
+            for (doc in value) {
+              // 9
+              val post = doc.toObject(Post::class.java)
+              posts.add(post)
+            }
+            // 10
+            postsValues.postValue(posts)
+          }
+        })
+  }
+
+  private fun getCurrentTime() = System.currentTimeMillis()
+}
---
+++ 19-reading-data-from-cloud-firestore/projects/final/app/src/main/java/com/raywenderlich/android/whatsup/ui/postDetails/PostDetailsActivity.kt
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.whatsup.ui.postDetails
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.raywenderlich.android.whatsup.R
+import com.raywenderlich.android.whatsup.databinding.ActivityPostDetailsBinding
+import com.raywenderlich.android.whatsup.firebase.authentication.AuthenticationManager
+import com.raywenderlich.android.whatsup.firebase.firestore.CloudFirestoreManager
+import com.raywenderlich.android.whatsup.firebase.realtimeDatabase.RealtimeDatabaseManager
+import com.raywenderlich.android.whatsup.model.Comment
+import com.raywenderlich.android.whatsup.model.Post
+import com.raywenderlich.android.whatsup.util.DateUtils
+import com.raywenderlich.android.whatsup.util.showToast
+
+class PostDetailsActivity : AppCompatActivity() {
+
+  private var post: Post? = null
+
+  private val commentsAdapter by lazy { CommentsAdapter(DateUtils()) }
+  private val authenticationManager by lazy { AuthenticationManager() }
+  private val realtimeDatabaseManager by lazy { RealtimeDatabaseManager() }
+  private val cloudFirestoreManager by lazy { CloudFirestoreManager() }
+  private lateinit var postDetailsBinding: ActivityPostDetailsBinding
+
+  companion object {
+    private const val POST_EXTRA = "post_extra"
+    fun createIntent(context: Context, post: Post) = Intent(context, PostDetailsActivity::class.java).apply {
+      putExtra(POST_EXTRA, post)
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_post_details)
+    initialize()
+  }
+
+  override fun onStart() {
+    super.onStart()
+    listenForComments()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    cloudFirestoreManager.stopListeningForCommentsChanges()
+  }
+
+  private fun initialize() {
+    setSupportActionBar(postDetailsBinding.postDetailsToolbar)
+    extractArguments()
+    initializeClickListener()
+    initializeRecyclerView()
+    if (authenticationManager.getCurrentUser() != post?.author) {
+      postDetailsBinding.updatePostButton.visibility = View.GONE
+      postDetailsBinding.deletePostButton.visibility = View.GONE
+    }
+
+    postDetailsBinding.postText.setText(post?.content, TextView.BufferType.EDITABLE)
+  }
+
+  private fun initializeRecyclerView() {
+    postDetailsBinding.commentsRecyclerView.layoutManager = LinearLayoutManager(this)
+    postDetailsBinding.commentsRecyclerView.setHasFixedSize(true)
+    postDetailsBinding.commentsRecyclerView.adapter = commentsAdapter
+
+    val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+    postDetailsBinding.commentsRecyclerView.addItemDecoration(divider)
+  }
+
+  private fun listenForComments() {
+    post?.id?.let { postId ->
+      cloudFirestoreManager.onCommentsValuesChange(postId)
+          .observe(this, Observer(::onCommentsUpdate))
+    }
+  }
+
+  private fun onCommentsUpdate(comments: List<Comment>) {
+    commentsAdapter.onCommentsUpdate(comments)
+  }
+
+  private fun initializeClickListener() {
+    post?.let { post ->
+      postDetailsBinding.updatePostButton.setOnClickListener {
+        cloudFirestoreManager.updatePostContent(
+            post.id,
+            postDetailsBinding.postText.text.toString().trim(),
+            ::onPostSuccessfullyUpdated,
+            ::onPostUpdateFailed
+        )
+      }
+
+      postDetailsBinding.deletePostButton.setOnClickListener {
+        cloudFirestoreManager.deletePost(post.id, ::onPostSuccessfullyDeleted, ::onPostDeleteFailed)
+      }
+
+      postDetailsBinding.addCommentButton.setOnClickListener {
+        val comment = postDetailsBinding.commentEditText.text.toString().trim()
+        if (comment.isNotEmpty()) {
+          cloudFirestoreManager.addComment(
+              post.id,
+              comment,
+              ::onCommentSuccessfullyAdded,
+              ::onCommentAddFailed
+          )
+        } else {
+          showToast(getString(R.string.empty_comment_message))
+        }
+      }
+    }
+  }
+
+  private fun onPostSuccessfullyUpdated() {
+    showToast(getString(R.string.post_updated_successfully))
+    finish()
+  }
+
+  private fun onPostUpdateFailed() {
+    showToast(getString(R.string.post_update_failed))
+  }
+
+  private fun onPostSuccessfullyDeleted() {
+    showToast(getString(R.string.post_successfully_deleted))
+    finish()
+  }
+
+  private fun onPostDeleteFailed() {
+    showToast(getString(R.string.post_delete_failed))
+  }
+
+  private fun onCommentSuccessfullyAdded() {
+    postDetailsBinding.commentEditText.text.clear()
+  }
+
+  private fun onCommentAddFailed() {
+    showToast(getString(R.string.comment_add_failed))
+  }
+
+  private fun extractArguments() {
+    post = intent.getParcelableExtra(POST_EXTRA)
+  }
+}
