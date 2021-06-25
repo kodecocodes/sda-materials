c213104db0db4d1be59dac9bc4868c99725506ac
Fuad Kamal <fuad@anaara.com>
2021-06-25 15:56:28 -0400

    update copyright

--- 02-using-sharedpreferences/projects/final/app/src/main/java/com/raywenderlich/android/organizedsimplenotes/Note.kt
+++ 02-using-sharedpreferences/projects/final/app/src/main/java/com/raywenderlich/android/organizedsimplenotes/Note.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
  */
 
 package com.raywenderlich.android.organizedsimplenotes
