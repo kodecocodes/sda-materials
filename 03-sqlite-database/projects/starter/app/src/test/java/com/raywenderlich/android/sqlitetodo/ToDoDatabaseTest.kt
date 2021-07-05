972470b13e723998af37f5178ec0c7d5628c20a8
Fuad Kamal <fuad@anaara.com>
2021-07-05 16:11:00 -0400

    updated dependencies
    Update dependencies
    Create test / test directory for unit test

--- 03-sqlite-database/projects/starter/app/src/test/java/com/raywenderlich/android/sqlitetodo/ToDoDatabaseTest.kt
+++ 03-sqlite-database/projects/starter/app/src/test/java/com/raywenderlich/android/sqlitetodo/ToDoDatabaseTest.kt
@@ -30,56 +30,5 @@
 
 package com.raywenderlich.android.sqlitetodo
 
-import androidx.test.core.app.ApplicationProvider
-import com.raywenderlich.android.sqlitetodo.controller.ToDoDatabaseHandler
-import com.raywenderlich.android.sqlitetodo.model.ToDo
-import org.junit.After
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.RuntimeEnvironment
-
-@RunWith(RobolectricTestRunner::class)
-class TestDatabase {
-  // 1
-  private lateinit var dbHelper: ToDoDatabaseHandler
-
-  // 2
-  @Before
-  fun setup() {
-    dbHelper = ToDoDatabaseHandler(ApplicationProvider.getApplicationContext())
-    dbHelper.recreateEmptyDatabase()
-  }
-
-
-  @Test
-  @Throws(Exception::class)
-  fun testDatabaseInsertion() {
-
-    // 3
-    // Given
-    val item1 = ToDo(0, "Test my Program", false)
-    val item2 = ToDo(0, "Test my Program Again", false)
-
-    // 4
-    // When
-    dbHelper.createToDo(item1)
-    dbHelper.createToDo(item2)
-
-    // 5
-    // Then
-    val allText = dbHelper.getAllText()
-    val expectedValue = "${item1.toDoName}${item2.toDoName}"
-    Assert.assertEquals(allText, expectedValue)
-  }
-
-  // 6
-  @After
-  fun reset() {
-    dbHelper.clearDatabase()
-  }
+class ToDoDatabaseTest {
 }
-
-
