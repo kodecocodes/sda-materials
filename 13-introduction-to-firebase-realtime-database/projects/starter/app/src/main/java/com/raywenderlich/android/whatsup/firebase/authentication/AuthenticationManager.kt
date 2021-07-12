aff7c0894bdb13a8749fcb54683a531f71eff147
Fuad Kamal <fuad@anaara.com>
2021-07-12 00:04:37 -0400

    refactor package
    add android package

---
+++ 13-introduction-to-firebase-realtime-database/projects/starter/app/src/main/java/com/raywenderlich/android/whatsup/firebase/authentication/AuthenticationManager.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.whatsup.firebase.authentication
+
+import android.app.Activity
+import android.content.Context
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+const val RC_SIGN_IN = 1000
+
+class AuthenticationManager {
+
+  /*
+  private val firebaseAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+  private val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+  fun startSignInFlow(activity: Activity) {
+    activity.startActivityForResult(
+        AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(false)
+            .build(),
+        RC_SIGN_IN
+    )
+  }
+
+  fun isUserSignedIn() = firebaseAuth.currentUser != null
+
+  fun getCurrentUser() = firebaseAuth.currentUser
+
+  fun signOut(context: Context) {
+    val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+    val googleSignInClient = GoogleSignIn.getClient(context, googleSignInOptions)
+    firebaseAuth.signOut()
+    googleSignInClient.signOut()
+  }
+  */
+}
