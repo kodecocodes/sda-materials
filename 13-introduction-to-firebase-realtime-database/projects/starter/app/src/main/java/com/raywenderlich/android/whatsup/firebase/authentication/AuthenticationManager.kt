6f0ae6b5dea95eb3f5bc971abe190c6d29dcd55c
Fuad Kamal <fuad@anaara.com>
2021-07-12 00:14:19 -0400

    update copyright

--- 13-introduction-to-firebase-realtime-database/projects/starter/app/src/main/java/com/raywenderlich/android/whatsup/firebase/authentication/AuthenticationManager.kt
+++ 13-introduction-to-firebase-realtime-database/projects/starter/app/src/main/java/com/raywenderlich/android/whatsup/firebase/authentication/AuthenticationManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,6 +10,14 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
