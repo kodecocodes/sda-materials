d9a52df8fa34aa0e738fb2c58a736359b9ae7987
Gabriela <gabriela@codecons.com>
2021-07-21 21:48:51 +0200

    TE - update projects with the splash screen, app icons and fonts

---
+++ 21-cloud-storage/projects/final/app/src/main/java/com/raywenderlich/android/whatsup/ui/Router.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.whatsup.ui
+
+import android.app.Activity
+import com.raywenderlich.android.whatsup.ui.home.HomeActivity
+import com.raywenderlich.android.whatsup.ui.login.LoginActivity
+
+class Router {
+
+  fun startHomeScreen(activity: Activity) {
+    val intent = HomeActivity.createIntent(activity)
+    activity.startActivity(intent)
+  }
+
+  fun startLoginScreen(activity: Activity) {
+    val intent = LoginActivity.createIntent(activity)
+    activity.startActivity(intent)
+  }
+}
---
+++ 21-cloud-storage/projects/final/app/src/main/java/com/raywenderlich/android/whatsup/ui/home/HomeActivity.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2021 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.whatsup.ui.home
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.raywenderlich.android.whatsup.R
+import com.raywenderlich.android.whatsup.firebase.authentication.AuthenticationManager
+import com.raywenderlich.android.whatsup.firebase.cloudStorage.CloudStorageManager
+import com.raywenderlich.android.whatsup.ui.Router
+import com.raywenderlich.android.whatsup.util.ImageLoader
+import com.raywenderlich.android.whatsup.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+  private val authenticationManager by lazy { AuthenticationManager() }
+  private val cloudStorageManager by lazy { CloudStorageManager() }
+
+  private val router by lazy { Router() }
+  private val imageLoader by lazy { ImageLoader() }
+  private lateinit var binding: ActivityHomeBinding
+
+  companion object {
+    private const val IMAGE_TYPE = "image/jpeg"
+    fun createIntent(context: Context) = Intent(context, HomeActivity::class.java)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityHomeBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    initialize()
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_home, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean =
+      when (item.itemId) {
+        R.id.action_logout -> {
+          authenticationManager.signOut(this)
+          router.startLoginScreen(this)
+          finish()
+          true
+        }
+        else -> super.onOptionsItemSelected(item)
+      }
+
+  private fun initialize() {
+    setSupportActionBar(binding.homeToolbar)
+
+    binding.addPostFab.setOnClickListener {
+      pickImages.launch(IMAGE_TYPE)
+    }
+  }
+
+  private fun onPhotoUploadSuccess(url: String) {
+    binding.progressbar.visibility = View.GONE
+    imageLoader.loadImage(this, url, binding.imageView)
+  }
+
+  private val pickImages = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+    uri?.let { selectedImageUri ->
+      binding.progressbar.visibility = View.VISIBLE
+      cloudStorageManager.uploadPhoto(selectedImageUri, ::onPhotoUploadSuccess)
+    }
+  }
+}
