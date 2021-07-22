466fd4061a04ae87ed8be4e9705360355b6ac31f
Gabriela <gabriela@codecons.com>
2021-07-22 22:18:10 +0200

    TE - updated the starter project

--- 08-mastering-relations/projects/starter/app/src/main/java/com/raywenderlich/android/droidquiz/data/model/QuestionAndAllAnswers.kt
+++ 08-mastering-relations/projects/starter/app/src/main/java/com/raywenderlich/android/droidquiz/data/model/QuestionAndAllAnswers.kt
@@ -3,14 +3,6 @@ package com.raywenderlich.android.droidquiz.data.model
 import androidx.room.Embedded
 import androidx.room.Relation
 
-
 class QuestionAndAllAnswers {
-    @Embedded//1
-    var question: Question? = null
 
-    @Relation(
-        parentColumn = "question_id",//2
-        entityColumn = "question_id"
-    )
-    var answers: List<Answer> = ArrayList()//3
 }
